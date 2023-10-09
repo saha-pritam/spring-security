eb569aad2803d5a56e8e494441d34fbf05672b25
Pritam Saha <saha.pritam.work@gmail.com>
2023-10-09 15:52:56 +0530

    Topic-13

---
+++ SpringSecurity/src/main/java/org/pritam/springsecurity/controller/MyController.java
@@ -0,0 +1,20 @@
+package org.pritam.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	@GetMapping("/one")
+	public String one() {
+		return "<h1>Hi I am One.</h1>";
+	}
+	@GetMapping("/two")
+	public String two() {
+		return "<h1>Hi I am Two.</h1>";
+	}
+	@GetMapping("/three")
+	public String three() {
+		return "<h1>Hi I am Three.</h1>";
+	}
+}
